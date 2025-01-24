f87974da9049a06fd5dfd7a2b28ea292245251ba
Hazz-i <wahidh697@gmail.com>
2025-01-24 16:45:02 +0700

    update enc

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ CORS(app)
 db = mysql.connector.connect(
     host=os.getenv('DB_HOST'),
     user=os.getenv('DB_USER'),
+    port=int(os.getenv('DB_PORT', 3306)),
     password=os.getenv('DB_PASSWORD'),
     database=os.getenv('DB_NAME')
 )
